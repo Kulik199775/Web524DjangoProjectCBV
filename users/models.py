168a27476da0b98661ea5f75ad9e0c5daa73cb98
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-03-03 19:22:10 +0300

    1) Внесены изменения в отображаемые поля моделей

--- users/models.py
+++ users/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import AbstractUser
 NULLABLE = {'blank': True, 'null': True}
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name='email')
-    first_name = models.CharField(max_length=150, verbose_name='first_name', default='Anonymous')
-    last_name = models.CharField(max_length=150, verbose_name='last_name', default='Anonymous')
-    avatar = models.ImageField(upload_to='users/', verbose_name='avatar', **NULLABLE)
-    phone = models.CharField(max_length=35, verbose_name='phone', **NULLABLE)
-    telegram = models.CharField(max_length=150, verbose_name='telegram username', **NULLABLE)
-    is_active = models.BooleanField(default=True, verbose_name='active')
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+    first_name = models.CharField(max_length=150, verbose_name='Имя', default='Anonymous')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия', default='Anonymous')
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name='Номер телефона', **NULLABLE)
+    telegram = models.CharField(max_length=150, verbose_name='Аккаунт телеграм', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='Статус аккаунта')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
