6966e95519bd4594a27743b039b029cac638985c
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-03-05 13:46:34 +0300

    1) Добавлен ReviewListView 2) Добавлен ReviewDeactivatedListView 3)Созданы шаблоны для Reviews 4) Внесены необходимые изменения в текущие шаблоны

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.shortcuts import reverse, get_object_or_404, redirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
+from redis.commands.search.querystring import querystring
 
-# Create your views here.
+from reviews.models import Review
+from reviews.forms import ReviewForm
+from users.models import User
+
+
+class ReviewListView(ListView):
+    model = Review
+    extra_context = {
+        'title': 'Наши отзывы',
+    }
+    template_name = 'reviews/reviews.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(sign_of_review=True)
+        return queryset
+
+
+class ReviewDeactivatedListView(ListView):
+    model = Review
+    extra_context = {
+        'title': 'Неактивные отзывы',
+    }
+    template_name = 'reviews/reviews.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(sign_of_review=False)
+        return queryset
